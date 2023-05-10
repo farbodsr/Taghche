5ba3293a600b1bf927abfedbab9528c93bb8eb15
farbod saraei <farbodsr@outlook.com>
2023-05-10 22:53:50 +0430

    Add Dependencyinjection Setup to InmemotyCache

---
+++ src/Adaptors/InMemoryCacheAdaptor/DISetup/DISetup.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationCore.Ports;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InMemoryCacheAdaptor.DISetup
+{
+    public static class DISetup
+    {
+        public static IServiceCollection AddInMemoryCacheService(this IServiceCollection services) 
+        {
+            services.AddSingleton<IInMemoryCache, CacheManager>();
+            return new ServiceCollection(); 
+        }
+    }
+}
