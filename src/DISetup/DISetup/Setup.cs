1f4869ae8ac9bebcb1f1b0e77ce82cade079b0de
farbod saraei <farbodsr@outlook.com>
2023-05-10 22:54:09 +0430

    Add DI Setup to project

---
+++ src/DISetup/DISetup/Setup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using InMemoryCacheAdaptor.DISetup;
+
+namespace DISetup
+{
+    public static class Setup
+    {
+        public static IServiceCollection AddDependencies(this IServiceCollection services)
+        {
+            services.AddInMemoryCacheService();
+            return new ServiceCollection();
+        }
+    }
+}
