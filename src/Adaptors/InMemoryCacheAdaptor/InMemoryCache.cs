975576a29ea883f25252d2278d31b4bea4a0fe76
farbod saraei <farbodsr@outlook.com>
2023-05-10 16:28:14 +0430

    Add InMemory Adaptor

---
+++ src/Adaptors/InMemoryCacheAdaptor/InMemoryCache.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryCacheAdaptor
+{
+    public interface ICacheManager
+    {
+        T Get<T>(string key);
+        void Set<T>(string key, T value, TimeSpan expirationTime);
+        void Remove(string key);
+        void Clear();
+    }
+
+    public class CacheManager : ICacheManager
+    {
+        private readonly MemoryCache _cache;
+
+        public CacheManager()
+        {
+            _cache = new MemoryCache(new MemoryCacheOptions());
+        }
+
+        public T Get<T>(string key)
+        {
+            return _cache.Get<T>(key);
+        }
+
+        public void Set<T>(string key, T value, TimeSpan expirationTime)
+        {
+            var cacheEntryOptions = new MemoryCacheEntryOptions()
+                .SetSlidingExpiration(expirationTime);
+
+            _cache.Set<T>(key, value, cacheEntryOptions);
+        }
+
+        public void Remove(string key)
+        {
+            _cache.Remove(key);
+        }
+
+        public void Clear()
+        {
+            _cache.Clear();
+        }
+    }
+
+}
